b9c9a02aa9ba7b331c0d070f22cf05f05e9286c4
Nikoloz97 <nick.gotsy@gmail.com>
2023-02-12 09:05:40 -0500

    Cleared out default Vue3 data

--- src/components/Main/Main.vue
+++ src/components/Main/Main.vue
@@ -1,24 +1,14 @@
 <script setup>
-import { ref } from 'vue'
+
 
 defineProps({
-  msg: String,
+ 
 })
 
-const count = ref(0)
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
 
   <div class="container">
   <div class="row">
@@ -33,6 +23,7 @@ const count = ref(0)
     </div>
   </div>
 </div>
+
 
 </template>
 
