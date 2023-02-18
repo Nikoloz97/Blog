67bdd44899410d6fc83ca3d4b46519b8887d14ca
Nikoloz97 <nick.gotsy@gmail.com>
2023-02-17 22:13:19 -0500

    Created mock json database

--- src/components/Travel/AllPosts.vue
+++ src/components/Travel/AllPosts.vue
@@ -1,10 +1,42 @@
 <template>
+    <div class="allPosts">
 
-    <p>This is the footer!</p>
+        <form @submit.prevent="handleSubmit">
+        <input 
+            type="text"
+            placeholder="Add another country..."
+            v-model="newCountry">
+
+            <button>Add</button>
+        </form>
+
+        <h2>List of Countries I've been to</h2>
+        <ul v-for="countries in travelStore.countriesVisited">
+            <li>{{ countries }}</li>
+        </ul>
+
+        <p>Latest country I've been to is {{ travelStore.getLatestCountry}}</p>
+        
+
+
+    </div>
 
 </template>
 
-<script>
+<!-- With setup keyword, don't need to export default, setup, and return statements -->
+<script setup>
+import {useTravelStore} from '../../stores/TravelStore'
+import { ref } from 'vue';
+
+const travelStore = useTravelStore()
+const newCountry = ref('')
+const handleSubmit = () => {
+    travelStore.addCountry(newCountry.value)
+    // Once country added to list, reset the newCountry value 
+    newCountry.value = ''
+
+}
+
 </script>
 
 
