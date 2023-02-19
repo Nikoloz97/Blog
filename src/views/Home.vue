06fefb38fdb364aa2d54276a2d6420d2ee348b04
Nikoloz97 <nick.gotsy@gmail.com>
2023-02-18 21:40:54 -0500

    Home slideshow skeleton

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,5 @@
 <template>  
 
-<router-link to="/Coding"><button>Coding</button></router-link>
-<router-link to="/Health"><button>Health</button></router-link>
-<router-link to="/Travel"><button>Travel</button></router-link>
-
 <slide-show/>
 <all-posts/>
 
